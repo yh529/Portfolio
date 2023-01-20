3c43759e3f1093c3125063c4b9ad0d2341291aae
yh529 <yudanhdunn@gmail.com>
2023-01-19 21:47:30 -0500

    Added Interations

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello! Welcome to My Website!");
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
